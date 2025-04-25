d2a8ece1048ed6699f43978ae70e11a8e769f779
Jayani <s20170@sci.pdn.ac.lk>
2025-04-25 16:08:26 +0530

    Assignment commited

--- WebScraper.java
+++ WebScraper.java
@@ -5,5 +5,11 @@ import java.io.IOException;
 public class WebScraper {
     public static void main(String[] args) {
         // TODO: Scrape a URL and print its title
+
+    String url="https://bbc.com";
+    Document doc = Jsoup.connect(url).get();
+    
     }
+
+
 }
